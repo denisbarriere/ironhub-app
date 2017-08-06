2f1e40b53fe5565daf5e4bd97dc82795dcb93d20
Denis Barriere <denis.barriere@gmail.com>
2017-08-06 14:05:23 +0200

    Add project-contributors component

---
+++ src/app/components/project-contributors/project-contributors.component.ts
@@ -0,0 +1,19 @@
+// Angular
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-project-contributors',
+  templateUrl: './project-contributors.component.html',
+  styleUrls: ['./project-contributors.component.scss']
+})
+export class ProjectContributorsComponent implements OnInit {
+
+ // Retrieve the relevant information from the parent component
+  @Input() contributors: Array<Object>;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
