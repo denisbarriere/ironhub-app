5ca776e3f4ffe02d5ecc826ef12b333398e4c817
Denis Barriere <denis.barriere@gmail.com>
2017-08-06 14:06:43 +0200

    Add project-list-item component

---
+++ src/app/components/project-list-item/project-list-item.component.ts
@@ -0,0 +1,18 @@
+//Angular
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-project-list-item',
+  templateUrl: './project-list-item.component.html',
+  styleUrls: ['./project-list-item.component.scss']
+})
+export class ProjectListItemComponent implements OnInit {
+
+  @Input() project: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
