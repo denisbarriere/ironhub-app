6fb5145251c42038f1f9a99e6f4e9748864e858e
Denis Barriere <denis.barriere@gmail.com>
2017-08-06 14:07:06 +0200

    Update project-list component to include new child components

--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 
 // services
 import { ProjectService } from '../../services/project.service';
+import { SessionService } from '../../services/session.service';
 
 @Component({
   selector: 'app-project-list',
@@ -11,29 +12,58 @@ import { ProjectService } from '../../services/project.service';
 })
 export class ProjectListComponent implements OnInit {
 
-  projects;
-  rowCount: Number;
+  projects = [{}]; // Retrive the projects from the API
+  numberOfRows: number = 0; // Number of rows in the project grid
+  numberOfColumns: number = 4; // Number of colums in the project grid
+  numberOfProjects: number = 0; // Total number of projects
+  projectsPerRow: Array<any> = []; // Array of project object (columns) per row
+  projectIndex: number = 0; // Index used to populate the projectsPerRow Array
 
   constructor(
-    private project: ProjectService
+    // Dependency injections
+    private project: ProjectService,
+    private session: SessionService
   ) { }
 
   ngOnInit() {
     this.project.getProjectList()
       .subscribe((projects) => {
         this.projects = projects;
-      });
-
-      
     
-    // this.rowCount = Math.floor(this.projects.length / 4);
-    //   if (this.projects.length % 4 > 0) {
-    //     rowCount += 1;
-    //   }
-    //   toolIndex = 0;
-    //   tool
-    //   for (let row = 0; row < rowCount; row++) {
+        // Retrieve the number of projects found
+        this.numberOfProjects = this.projects.length;
 
+        // Calculate the number of rows to display in the grid
+        this.numberOfRows = Math.round(this.projects.length / this.numberOfColumns);
 
+        /* Built the array used for the first ngFor. The number of elements is the number of rows.
+           Each element is an array including each project for this row
+           Example for 2 rows and 2 columns:
+            [
+              [ {project 1(row0/col0)}, {project 2(row0/col1)} ],
+              [ {project 3(row1/col0)}, {project 4(row1/col1)} ]
+            ]
+        */
+        for (let row = 0; row < this.numberOfRows; row += 1) {
+          
+          // Declare working array
+          let projectsToAddToRow = [];
+
+          // Build the array of projects per row
+          for (let col = 0; col < this.numberOfColumns; col += 1) {
+          
+            // If there is a project at this index, push it to the working array
+            if (this.projects[this.projectIndex]) {
+              projectsToAddToRow.push(this.projects[this.projectIndex]);
+            }
+
+            // Inscrease the project counter
+            this.projectIndex += 1;
+          }
+     
+          // Push the list of projects in the row to the appropriate array
+          this.projectsPerRow.push(projectsToAddToRow);
+        }
+      });
   }
 }
