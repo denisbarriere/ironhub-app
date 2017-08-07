6fe89c66e5e55f681fa7f1f251fdaa34d8ad9b7c
Denis Barriere <denis.barriere@gmail.com>
2017-08-07 13:54:45 +0200

    Add overlay and buttons based on who is logged in

--- src/app/components/project-list-item/project-list-item.component.ts
+++ src/app/components/project-list-item/project-list-item.component.ts
@@ -1,5 +1,13 @@
 //Angular
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+// Javacript libraries
+import * as _ from 'lodash';
+
+// services
+import { ProjectService } from '../../services/project.service';
+import { SessionService } from '../../services/session.service';
 
 @Component({
   selector: 'app-project-list-item',
@@ -9,10 +17,35 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProjectListItemComponent implements OnInit {
 
   @Input() project: any;
+  @Output() onProjectDelete = new EventEmitter<string>();
 
-  constructor() { }
+  isEditable: boolean; // Used to know if the actions (edit, delete) should be displayed
 
+  constructor(
+    private router: Router,
+    private projectService: ProjectService,
+    private session: SessionService
+  ) { }
+
+  
   ngOnInit() {
+    
+    // Check if the logged-in user is an admin or a contributor of the project
+    let isContributor = _.findKey(this.project.contributors, (contributor) => {
+      return contributor['_id'] === this.session.user['_id']
+    });
+    
+    // If so, the logged-in user can edit
+    if(this.session.user['role'] === 'ADMIN' || isContributor) { 
+      this.isEditable = true;
+    }
   }
 
+  // Delete the current project
+  deleteProject() {
+    if (window.confirm('Are you sure?')) {
+      // Propagate the event to the parent component
+      this.onProjectDelete.emit(this.project._id);
+    }
+  }
 }
