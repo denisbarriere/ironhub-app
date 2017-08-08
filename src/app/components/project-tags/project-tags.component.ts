4674c289a5016088de17d5200895af54b67f42a7
Denis Barriere <denis.barriere@gmail.com>
2017-08-08 20:07:38 +0200

    Improve tag component to handle project detail page

--- src/app/components/project-tags/project-tags.component.ts
+++ src/app/components/project-tags/project-tags.component.ts
@@ -9,12 +9,18 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProjectTagsComponent implements OnInit {
 
   // Retrieve the relevant information from the parent component
-  @Input() type: String;
   @Input() endOfModuleProject: String;
+  @Input() absolutePosition: String; // Used to know if the tag should be absolute positionned
+
+  position: String = ''; // position class
 
   constructor() { }
 
   ngOnInit() {
+    // Add the appropriate class if required (asked from the parent component)
+    if (this.absolutePosition) {
+      this.position = 'project-tag-absolute';
+    } 
   }
 
 }
