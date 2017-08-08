4b53cfc2bc8828af5154bf42fa1a094a83cb58d6
Denis Barriere <denis.barriere@gmail.com>
2017-08-08 21:21:31 +0200

    Make the component handle list and detail pages

--- src/app/components/project-contributors/project-contributors.component.ts
+++ src/app/components/project-contributors/project-contributors.component.ts
@@ -9,13 +9,23 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProjectContributorsComponent implements OnInit {
 
   // Retrieve the relevant information from the parent component
-  @Input() contributors: Array<Object>;
+  @Input() contributors: Array<Object>; // The list of contributors
+  @Input() fullList: boolean; // Used to know if the component should display the full list of contributors not not
 
-  maxNumberOfContributors = 4;
+  maxNumberOfContributors: number = 2; // Maximum number of contributors displayed on the project listing page
+  numberOfContributors: number = 0; // Total number of contributors for the project
 
   constructor() { }
 
   ngOnInit() {
+    // If the component has to display all the contributors, then set the maxNumberOfContributors to the number of contributors
+    if (!this.fullList) {
+      // Save the number of contributors
+      this.numberOfContributors = this.contributors.length;
+
+      // Take the first x contributors 
+      this.contributors.splice(this.maxNumberOfContributors, ); 
+    }
   }
 
 }
