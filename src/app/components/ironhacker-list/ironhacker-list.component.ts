1532ddacdd6650f127d8bf4f8e55961cb0dd721e
Denis Barriere <denis.barriere@gmail.com>
2017-08-09 11:32:16 +0200

    Start implementing ironhacker list

--- src/app/components/ironhacker-list/ironhacker-list.component.ts
+++ src/app/components/ironhacker-list/ironhacker-list.component.ts
@@ -1,4 +1,8 @@
+// Angular
 import { Component, OnInit } from '@angular/core';
+
+// services
+import { IronhackerService } from '../../services/ironhacker.service';
 
 @Component({
   selector: 'app-ironhacker-list',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ironhacker-list.component.scss']
 })
 export class IronhackerListComponent implements OnInit {
+  ironhackers = [{}]; // Retrive the ironhackers from the API
+  numberOfIronhackers: number = 0; // Total number of ironhackers
 
-  constructor() { }
+  constructor(
+    // Dependency injections
+    private ironhacker: IronhackerService
+  ) { }
 
   ngOnInit() {
-  }
+    
+    // Get the list of projects
+    this.ironhacker.getIronhackerList()
+      .subscribe((ironhackers) => {
+        this.ironhackers = ironhackers;
 
+        // Retrieve the number of ironhackers found
+        this.numberOfIronhackers = this.ironhackers.length;
+      });
+  }
 }
